a87b787d8c6c070bb774ca4ce35553eb3ec4f9ce
Suelen Karbivnychyy <suelenmatosr@gmail.com>
2023-09-15 13:34:58 -0400

    developed a try an except blocker to cacth if the user enter a value that is not a integer

--- game.py
+++ game.py
@@ -11,17 +11,23 @@ def guessing_game():
     game_is_on = True
 
     while game_is_on:
-        user_guess = int(input("Your guess? "))
-        if user_guess < 1 or user_guess > 100:
-            print("Hey bastard you enter a number that is not in the range a asked you! Try better next time.")
-        elif user_guess < computer_number:
-            attempt += 1
-            print("Your guess is too low, try again.\n")        
-        elif user_guess > computer_number: 
-            attempt += 1
-            print("Your guess is too high, try again.\n")
-        else:    
-            game_is_on = False        
-            return f"Well done, Jessica! You found my number in {attempt} tries!"            
+
+        try:
+            user_guess = int(input("Your guess? "))               
+        except ValueError:
+            print("\nYou bastard provided something that is not a number. Please provide a number next time.")     
+        else:
+            if user_guess < 1 or user_guess > 100:
+                print("\nHey bastard you enter a number that is not in the range a asked you! Try better next time.")     
+            elif user_guess < computer_number:
+                attempt += 1
+                print("Your guess is too low, try again.\n")        
+            elif user_guess > computer_number: 
+                attempt += 1
+                print("Your guess is too high, try again.\n")
+            else:    
+                game_is_on = False        
+                return f"Well done, {user_name}! You found my number in {attempt} tries!"     
+
 
 print(guessing_game())
